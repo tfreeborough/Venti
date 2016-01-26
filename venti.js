519a38b3a0f72562846d37a5b4f32d07b898f3a4
Thomas Freeborough <b2019162@my.shu.ac.uk>
2016-01-26 15:12:31 +0000

    Update venti.js
    added individual unlinking as well as event-wide unlinking, ability to view the event log from most recent backwards, as well as quality of life function nameFromFunction

--- venti.js
+++ venti.js
@@ -1,16 +1,45 @@
 var venti = {
+    "eventLogLimit": 1000,
     "registeredEvents" : {},
+    "log": [],
     emit: function (str) {
         'use strict';
         var args = Array.prototype.splice.call(arguments, 1);
         var event = this.registeredEvents[str];
-        event.forEach(function(e,i,a){
-            e.apply(null, args);
-        });
+        if(typeof event !== 'undefined'){
+            var that = this;
+            event.forEach(function(e,i,a){
+                e.apply(null, args);
+                that.log.push({event:str,callback: that.nameFromFunction(e)});
+            });
+        }else{
+            console.warn('Venti - Notice: You tried to emit an event ('+str+') which is not registered.');
+        }
+    },
+    nameFromFunction: function(callback){
+        var ret = callback.toString();
+        ret = ret.substr('function '.length);
+        return ret.substr(0, ret.indexOf('('));
+    },
+    eventLog: function(limit){
+        var l = this.eventLogLimit;
+        if(typeof limit !== 'undefined'){ l = limit }
+        return this.log.reverse().splice(0,limit);
     },
     link: function (str, callback) {
         'use strict';
         if(typeof this.registeredEvents[str] === 'undefined'){ this.registeredEvents[str] = []; }
         this.registeredEvents[str].push(callback);
+    },
+    unlink: function (str, callback) {
+        'use strict';
+        if(typeof callback !== 'undefined'){
+            if(typeof this.registeredEvents[str] !== 'undefined'){
+                delete this.registeredEvents[str][this.registeredEvents[str].indexOf(callback)];
+            }
+        }else{
+            if(typeof this.registeredEvents[str] !== 'undefined'){ delete this.registeredEvents[str]; }
+        }
+
     }
 };
